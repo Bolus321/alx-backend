41de10591cf0450ba7afc5a25b0bde0993f0bffd
Bolus321 <nnamdiogbolu321@gmail.com>
2024-11-05 18:31:33 +0000

    i18n

---
+++ 0x02-i18n/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Configuration  model fori  babel """
+
+
+class Config(object):
+    """Config class for  babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
